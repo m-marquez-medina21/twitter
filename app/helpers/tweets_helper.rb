5ca6df13bfbad02a216bb5f70f0519716f5378f3
m-marquez-medina21 <m.marquez.medina21@gmail.com>
2021-08-27 23:12:55 -0400

    Add hashtag

--- app/helpers/tweets_helper.rb
+++ app/helpers/tweets_helper.rb
@@ -1,2 +1,14 @@
 module TweetsHelper
+
+    def hashtag(content)
+        format_content = content.split(" ")
+        tag =[]
+        format_content.each do |p|
+            if p.start_with?("#")
+                p = link_to p, controller: 'tweets', q: p
+            end
+            tag.push(p)
+        end
+        raw tag.join(" ")
+    end
 end
